a5b8bdeaf0535ecb1b658598c224f25dbe07870d
bharani-palani <tp.bharani@gmail.com>
2023-07-23 17:56:06 +0530

    removed unwanted files

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/**
+ *  @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  distDir: "build",
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
